dfbfb8b699a00d20b76af60ef20dc7127f031480
T-Mobile IoT <iot@t-mobile.nl>
2017-11-28 11:54:21 +0100

    Add files via upload

---
+++ Sodaq_NBIOT_power_on_for_upgrade.ino
@@ -0,0 +1,30 @@
+#include "Arduino.h"
+
+#define DEBUG_STREAM SerialUSB
+#define powerPin 7 
+
+#define rxPin 0
+#define txPin 1
+
+
+void setup() 
+{
+  // Turn the nb-iot module on
+  pinMode(powerPin, OUTPUT); 
+  digitalWrite(powerPin, HIGH);
+
+  pinMode(rxPin, INPUT); 
+  pinMode(txPin, INPUT); 
+
+  // Start communication
+  DEBUG_STREAM.begin(9600);
+  DEBUG_STREAM.write("Dummy sketch for upgrade");
+  
+}
+
+void loop()
+{
+  delay(5000);
+}  
+
+
